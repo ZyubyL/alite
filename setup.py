141a714ca792b93caa841bacfc6d61f08ecdb80c
ZyubyL <zyubyl@gmail.com>
2026-09-14 04:22:33 +0700

    build: switch to setuptools to build instead of uv_build

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
+import sys
 import sysconfig
 
-from setuptools import Extension, setup
+from setuptools import Extension, find_packages, setup
 
 python_inc = sysconfig.get_path("include")
 python_lib = sysconfig.get_config_var("LIBDIR")
 
+if sys.platform == "win32":
+    extra_compile_args = ["/std:c23"]
+    extra_link_args: list[str] = []
+else:
+    extra_compile_args = ["-flto", "-std=c23"]
+    extra_link_args = ["-flto"]
+
 setup(
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
     ext_modules=[
         Extension(
             "alite._alite",
@@ -16,10 +26,10 @@ setup(
                 "src/alite/_core/pool.c",
             ],
             include_dirs=[python_inc],
-            library_dirs=[python_lib],
+            library_dirs=[python_lib] if python_lib else [],
             libraries=["sqlite3"],
-            extra_compile_args=["-flto", "-std=c23"],
-            extra_link_args=["-flto"],
+            extra_compile_args=extra_compile_args,
+            extra_link_args=extra_link_args,
         )
-    ]
+    ],
 )
