63fdea9dff5427a2b8b5cfd14c6554befefa3463
ZyubyL <zyubyl@gmail.com>
2026-09-20 14:02:18 +0700

    fix: min pool_size check

--- src/alite/pool.py
+++ src/alite/pool.py
@@ -32,7 +32,7 @@ class AsyncPool:
     """
 
     def __init__(self, _pool: _Pool, pool_size: int = DEFAULT_POOL_SIZE) -> None:
-        if not 0 <= pool_size <= MAX_POOL_SIZE:
+        if not 1 <= pool_size <= MAX_POOL_SIZE:
             raise ValueError("pool_size must between 1 and %d.", MAX_POOL_SIZE)
         self._pool = _pool
         self._executor = ThreadPoolExecutor(
